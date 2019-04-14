8fa704863631bd6971b7f9386d12aaf8ffc3a5db
David Poindexter <drpoindexter@gmail.com>
2019-04-14 13:45:27 -0400

    Adds basic start of CRA with styled components

---
+++ client/src/MpgApp/MpgApp.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import styled, { keyframes, createGlobalStyle } from 'styled-components';
+import logo from './logo.svg';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+      monospace;
+  }
+`;
+
+const AppWrapper = styled.div`
+  text-align: center;
+`;
+
+const rotate360 = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const AppLogo = styled.img`
+  animation: ${rotate360} infinite 20s linear;
+  height: 40vmin;
+  &:hover {
+    animation: ${rotate360} infinite 3s linear;
+  }
+`;
+
+const AppHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const AppLink = styled.a`
+  color: #61dafb;
+`;
+
+const AppIntro = styled.p`
+  font-size: large;
+  code {
+    font-size: 1.3rem;
+  }
+`;
+
+class MpgApp extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <AppWrapper>
+          <AppHeader>
+            <AppLogo src={logo} alt="logo" />
+            <AppIntro>MPG APP!!</AppIntro>
+            <AppIntro>
+              Components styled with <code>styled-components</code>{' '}
+              <span aria-label="nail polish" role="img">
+                💅
+              </span>
+            </AppIntro>
+            <AppLink
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </AppLink>
+          </AppHeader>
+        </AppWrapper>
+        <GlobalStyle />
+      </React.Fragment>
+    );
+  }
+}
+
+export default MpgApp;
