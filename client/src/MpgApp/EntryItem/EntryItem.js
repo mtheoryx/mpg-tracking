ff7968282ca4510dd585bbc1cf8e6c2e4f768508
David Poindexter <drpoindexter@gmail.com>
2019-04-21 13:25:51 -0400

    Add derived data for display items

--- client/src/MpgApp/EntryItem/EntryItem.js
+++ client/src/MpgApp/EntryItem/EntryItem.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
-const EntryItem = ({ item }) => {
+const EntryItem = ({ item, prevItemOdometer }) => {
+  const miles = prevItemOdometer
+    ? item.odometer - prevItemOdometer
+    : item.odometer;
+
+  const mpg = (miles / item.fuel).toFixed(2);
+
+  const cpm = (item.cost / miles).toFixed(2);
+
   return (
-    <tr key={item.odometer}>
-      <td>{item.odometer}</td> <td>${item.cost}</td>
+    <tr>
+      <td>{item.date.toLocaleDateString()}</td>
+      <td>{item.odometer}</td>
+      <td>{miles}</td>
+      <td>{item.fuel}</td>
+      <td>${item.cost}</td>
+      <td>{mpg}</td>
+      <td>{cpm}</td>
     </tr>
   );
 };
