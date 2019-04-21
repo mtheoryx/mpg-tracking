0f6fcaaa826995d225c5d668659b4f9f4fdc06c6
David Poindexter <drpoindexter@gmail.com>
2019-04-21 13:26:22 -0400

    Create a richer row display of data

--- client/src/MpgApp/Display/Display.js
+++ client/src/MpgApp/Display/Display.js
@@ -3,29 +3,55 @@ import EntryItem from '../EntryItem/EntryItem';
 import styled from 'styled-components';
 
 const DisplayTable = styled.table`
+  width: 95vw;
+  margin: 0 auto;
   tbody tr:nth-child(odd) {
     background-color: #663399;
     color: #fff;
   }
 `;
 
-const Display = ({ items, count = 3 }) => {
+const Display = ({ items, count, limit }) => {
+  const itemsList = limit
+    ? [...items.slice(Math.max(items.length - count, 1))]
+    : [...items];
+
+  const headers = [
+    ...Object.keys(items[0]).slice(0, 2),
+    'miles',
+    ...Object.keys(items[0]).slice(2)
+  ]
+    .filter(k => k !== 'location')
+    .concat('mpg', '$PM');
+
   return (
     <React.Fragment>
       <DisplayTable>
         <thead>
           <tr>
-            {items.length > 0 &&
-              Object.keys(items[0]).map(k => (
-                <td>{k.split('')[0].toUpperCase() + k.slice(1)}</td>
+            {itemsList.length > 0 &&
+              headers.map((k, index) => (
+                <td key={index}>{k.split('')[0].toUpperCase() + k.slice(1)}</td>
               ))}
           </tr>
         </thead>
         <tbody>
           {items.length > 0 &&
-            [...items.slice(Math.max(items.length - count, 1))].map(item => (
-              <EntryItem item={item} />
-            ))}
+            itemsList
+              .sort((a, b) => {
+                if (a.odometer < b.odometer) return 1;
+                if (a.odometer > b.odometer) return -1;
+                return 0;
+              })
+              .map((item, index, items) => (
+                <EntryItem
+                  item={item}
+                  key={item.odometer}
+                  prevItemOdometer={
+                    items[index + 1] && items[index + 1].odometer
+                  }
+                />
+              ))}
         </tbody>
       </DisplayTable>
     </React.Fragment>
